70e0b6eeafd484c39dfef51c3a4968ce0ca5500e
Shiv-812 <88423015+Shiv-812@users.noreply.github.com>
2025-02-21 18:18:49 +0530

    Update route.ts

--- src/app/api/webhook/clerk/route.ts
+++ src/app/api/webhook/clerk/route.ts
@@ -49,7 +49,7 @@ export async function POST(req: Request) {
 
   // Do something with payload
   // For this guide, log payload to console
-  const { id } = evt.data
+  // const { id } = evt.data
   const eventType = evt.type
   
   if(eventType === 'user.created'){
