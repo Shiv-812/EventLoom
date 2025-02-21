1abdf48e6e67909cef4ee6c103753fd5483a1196
Shiv-812 <88423015+Shiv-812@users.noreply.github.com>
2025-02-21 19:03:34 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware({
   publicRoutes: [
     '/',
     '/events/:id',
@@ -14,8 +14,12 @@ export default authMiddleware({
     '/api/uploadthing'
   ]
 });
- 
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
- 
