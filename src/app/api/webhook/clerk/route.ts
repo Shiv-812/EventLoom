bb12a41f7e3b4bff615bbc70a2cd80e44f217a49
Shiv <sp195526@gmail.com>
2025-02-28 22:41:14 +0530

    webhookroute

--- src/app/api/webhook/clerk/route.ts
+++ src/app/api/webhook/clerk/route.ts
@@ -5,7 +5,6 @@ import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
-<<<<<<< HEAD
   try {
     const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
 
@@ -15,65 +14,6 @@ export async function POST(req: Request) {
         { error: 'Error: Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local' }, 
         { status: 500 }
       )
-=======
-  const SIGNING_SECRET = process.env.SIGNING_SECRET
-
-  if (!SIGNING_SECRET) {
-    throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local')
-  }
-
-  // Create new Svix instance with secret
-  const wh = new Webhook(SIGNING_SECRET)
-
-  // Get headers
-  const headerPayload = headers()
-  const svix_id = headerPayload.get('svix-id')
-  const svix_timestamp = headerPayload.get('svix-timestamp')
-  const svix_signature = headerPayload.get('svix-signature')
-
-  // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error: Missing Svix headers', {
-      status: 400,
-    })
-  }
-
-  // Get body
-  const payload = await req.json()
-  const body = JSON.stringify(payload)
-
-  let evt: WebhookEvent
-
-  // Verify payload with headers
-  try {
-    evt = wh.verify(body, {
-      'svix-id': svix_id,
-      'svix-timestamp': svix_timestamp,
-      'svix-signature': svix_signature,
-    }) as WebhookEvent
-  } catch (err) {
-    console.error('Error: Could not verify webhook:', err)
-    return new Response('Error: Verification error', {
-      status: 400,
-    })
-  }
-
-  // Do something with payload
-  // For this guide, log payload to console
-  // const { id } = evt.data
-  const eventType = evt.type
-  
-  if(eventType === 'user.created'){
-    const {id, email_addresses, image_url, first_name, last_name, username} = evt.data;
-
-    const user = {
-        clerkId: id,
-        email: email_addresses[0].email_address,
-        username: username!,
-        firstName: first_name!,
-        lastName: last_name!,
-        photo: image_url,
->>>>>>> ef2ce2b50e2d0c02e13ec3d7858c6cd6ae3f0aff
     }
 
     console.log("❤️route trigger for webhook")
@@ -223,10 +163,4 @@ export async function POST(req: Request) {
       { status: 500 }
     )
   }
-<<<<<<< HEAD
 }
-=======
-
-  return new Response('Webhook received', { status: 200 })
-}
->>>>>>> ef2ce2b50e2d0c02e13ec3d7858c6cd6ae3f0aff
